c76231a581fd7c302aa64a89e1dafe0715b9eea3
Sayantan Kapat <ksayantan1823@gmail.com>
2021-04-09 23:23:27 +0530

    intial setup completed with 3 user

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
-
-function App() {
+import { ChatEngine } from 'react-chat-engine';
+import './App.css'
+const App = () => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <ChatEngine
+      height='100vh'
+      projectID='f98a774f-d40a-4fa0-8aa0-d6cf65a0eee9'
+      userName='Sayantan'
+      userSecret='123456'
+    />
   );
 }
 
